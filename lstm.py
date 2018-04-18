121660c1a6ad35fd200294e80e01014ce3878534
Gilles Feyrit <gilles.feyrit@gmail.com>
2018-04-18 08:32:43 +0200

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -12,78 +12,56 @@ from keras.layers import Activation
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
 
-def train_network():
-    """ Train a Neural Network to generate music """
-    notes = get_notes()
-
-    # get amount of pitch names
-    n_vocab = len(set(notes))
-
-    network_input, network_output = prepare_sequences(notes, n_vocab)
-
-    model = create_network(network_input, n_vocab)
-
-    train(model, network_input, network_output)
-
 def get_notes():
     """ Get all the notes and chords from the MIDI files in the ./midi_songs directory """
     notes = []
-    
     # Iterate all MIDI files using:
     # glob.glob(*pathname*)
     # Return a list of path names that match *pathname*.
     for file in glob.glob("midi_songs/*.mid"):
         midi = converter.parse(file)
-
+        # ...
         notes_to_parse = None
-
+        # ...
         tracks = instrument.partitionByInstrument(midi)
-
-        if parts:    # File has several instrument tracks.
+        if tracks:    # File has several instrument tracks.
             notes_to_parse = tracks.parts[0].recurse()    # Extract the 1st track (supposedly the melody).
         else:    # File has a single track.
             notes_to_parse = midi.flat.notes
-
+        # Parse the note events to either melody notes or chords.
         for element in notes_to_parse:
             if isinstance(element, note.Note):
                 notes.append(str(element.pitch))
             elif isinstance(element, chord.Chord):    # Code the chords like "note1.note2.etc"
                 notes.append('.'.join(str(n) for n in element.normalOrder))
-
+    # ...
     with open('data/notes', 'wb') as filepath:
         pickle.dump(notes, filepath)
-
     return notes
 
 def prepare_sequences(notes, n_vocab):
     """ Prepare the sequences used by the Neural Network """
     sequence_length = 100
-
     # get all pitch names
     pitchnames = sorted(set(item for item in notes))
-
      # create a dictionary to map pitches to integers
     note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
-
     network_input = []
     network_output = []
-
     # create input sequences and the corresponding outputs
     for i in range(0, len(notes) - sequence_length, 1):
         sequence_in = notes[i:i + sequence_length]
         sequence_out = notes[i + sequence_length]
         network_input.append([note_to_int[char] for char in sequence_in])
         network_output.append(note_to_int[sequence_out])
-
+    # ...
     n_patterns = len(network_input)
-
     # reshape the input into a format compatible with LSTM layers
     network_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
     # normalize input
     network_input = network_input / float(n_vocab)
-
+    # ...
     network_output = np_utils.to_categorical(network_output)
-
     return (network_input, network_output)
 
 def create_network(network_input, n_vocab):
@@ -103,7 +81,6 @@ def create_network(network_input, n_vocab):
     model.add(Dense(n_vocab))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-
     return model
 
 def train(model, network_input, network_output):
@@ -117,8 +94,20 @@ def train(model, network_input, network_output):
         mode='min'
     )
     callbacks_list = [checkpoint]
-
+    # ...
     model.fit(network_input, network_output, epochs=200, batch_size=64, callbacks=callbacks_list)
+
+def train_network():
+    """ Train a Neural Network to generate music. """
+    notes = get_notes()
+    # Get number of pitches in the training dataset.
+    n_vocab = len(set(notes))
+    # ...
+    network_input, network_output = prepare_sequences(notes, n_vocab)
+    # ...
+    model = create_network(network_input, n_vocab)
+    # ...
+    train(model, network_input, network_output)
 
 if __name__ == '__main__':
     train_network()
